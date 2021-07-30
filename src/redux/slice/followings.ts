b260bfae3a9efa992760b70746efaded53257e72
Allen Liu <allen6qi@gmail.com>
2021-07-30 22:34:38 +0800

    added userPostSlice and followingsSlice

---
+++ src/redux/slice/followings.ts
@@ -0,0 +1,48 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+
+export const initialState = {
+  loading: false,
+  hasErrors: false,
+  followings: [],
+};
+
+//Async Thunk Action
+export const fetchFollowings = createAsyncThunk(
+  'user/fetchFollowings', //name of your slice plus the name of thunk creator
+  async () => {
+    try {
+      const response = await fetch(
+        'http://localhost:8000/api/user/60ed4aa170b49b2b843f43d6/followings'
+      );
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      throw Error(`${error}`);
+    }
+  }
+);
+
+const { reducer } = createSlice({
+  name: 'followings',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchFollowings.fulfilled, (state, action) => {
+      state.followings = action.payload;
+      state.loading = false;
+      state.hasErrors = false;
+    });
+    builder.addCase(fetchFollowings.pending, (state) => {
+      state.loading = true;
+    });
+    builder.addCase(fetchFollowings.rejected, (state) => {
+      state.loading = false;
+      state.hasErrors = true;
+    });
+  },
+});
+
+export default reducer;
+
+//selector
+export const followingsSelector = (state) => state.followings.followings;
---
+++ src/redux/slice/userPosts.ts
@@ -0,0 +1,48 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+
+export const initialState = {
+  loading: false,
+  hasErrors: false,
+  userPosts: [],
+};
+
+//Async Thunk Action
+export const fetchUserPosts = createAsyncThunk(
+  'userPosts/fetchUserPosts', //name of your slice plus the name of thunk creator
+  async () => {
+    try {
+      const response = await fetch(
+        'http://localhost:8000/api/post/timeline/60ed4aa170b49b2b843f43d6'
+      );
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      throw Error(`${error}`);
+    }
+  }
+);
+
+const { reducer } = createSlice({
+  name: 'userPosts',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchUserPosts.fulfilled, (state, action) => {
+      state.userPosts = action.payload;
+      state.loading = false;
+      state.hasErrors = false;
+    });
+    builder.addCase(fetchUserPosts.pending, (state) => {
+      state.loading = true;
+    });
+    builder.addCase(fetchUserPosts.rejected, (state) => {
+      state.loading = false;
+      state.hasErrors = true;
+    });
+  },
+});
+
+export default reducer;
+
+//selector
+export const userPostsSelector = (state) => state.user.userPosts;
