63b18fa85a5a71534437a16cc7ee9f369649049f
Allen Liu <allen6qi@gmail.com>
2021-08-11 08:41:59 +0800

    added rootReducer to handle logoutUser action to reset all state

--- src/redux/rootReducer.ts
+++ src/redux/rootReducer.ts
@@ -5,11 +5,18 @@ import timelinePostsReducer from './slice/getTimelinePosts';
 import userPostsReducer from './slice/getUserPosts';
 import loginUserReducer from './slice/loginUser';
 
-const rootReducer = combineReducers({
+const combinedReducer = combineReducers({
   currentUser: loginUserReducer,
   followingUsers: followingUsersReducer,
   userPosts: userPostsReducer,
   timelinePosts: timelinePostsReducer,
 });
+
+const rootReducer = (state, action) => {
+  if (action.type === 'user/logoutUser') {
+    state = undefined;
+  }
+  return combinedReducer(state, action);
+};
 
 export default rootReducer;
