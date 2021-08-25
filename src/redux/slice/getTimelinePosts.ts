9df81fe9048d91f2d67ce3e433456d2b6b7451a9
Allen Liu <allen6qi@gmail.com>
2021-08-25 10:50:37 +0800

    added delete post thunk action

--- src/redux/slice/getTimelinePosts.ts
+++ src/redux/slice/getTimelinePosts.ts
@@ -6,7 +6,8 @@ export const initialState = {
   posts: [],
 };
 
-//Async Thunk Action
+//Async Thunk Action-----------------------------------------------------------------------------------
+//get all posts
 export const fetchTimelinePosts = createAsyncThunk(
   'timelinePosts/fetchTimelinePosts', //name of your slice plus the name of thunk creator
   async (id: string) => {
@@ -22,11 +23,27 @@ export const fetchTimelinePosts = createAsyncThunk(
   }
 );
 
+//delete post
+export const deletePost = createAsyncThunk(
+  'timelinePosts/deletePost', //name of your slice plus the name of thunk creator
+  async (id: string) => {
+    try {
+      await fetch(`http://localhost:8000/api/post/${id}/delete`, {
+        method: 'DELETE',
+      });
+      return id;
+    } catch (error) {
+      throw Error(`${error}`);
+    }
+  }
+);
+
 const { reducer } = createSlice({
   name: 'timelinePosts',
   initialState,
   reducers: {},
   extraReducers: (builder) => {
+    //fetchTimelinePosts----------------------------------------------------------------------------------------
     builder.addCase(fetchTimelinePosts.fulfilled, (state, action) => {
       state.posts = action.payload.sort((p1, p2) => {
         return new Date(p2.createdAt) - new Date(p1.createdAt);
@@ -38,6 +55,19 @@ const { reducer } = createSlice({
       state.loading = true;
     });
     builder.addCase(fetchTimelinePosts.rejected, (state) => {
+      state.loading = false;
+      state.hasErrors = true;
+    });
+    //deletePost-------------------------------------------------------------------------------------------------
+    builder.addCase(deletePost.fulfilled, (state, { payload }) => {
+      state.posts = state.posts.filter((post) => post._id !== payload);
+      state.loading = false;
+      state.hasErrors = false;
+    });
+    builder.addCase(deletePost.pending, (state) => {
+      state.loading = true;
+    });
+    builder.addCase(deletePost.rejected, (state) => {
       state.loading = false;
       state.hasErrors = true;
     });
