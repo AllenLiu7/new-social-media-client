7d8ac138c37f5a66e1c96acb9540874a5a0b4515
Allen Liu <allen6qi@gmail.com>
2021-08-31 13:48:13 +0800

    added nested persist reducer and remove it from local storage after logout

--- src/redux/rootReducer.ts
+++ src/redux/rootReducer.ts
@@ -1,12 +1,20 @@
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import followingUsersReducer from './slice/getFollowingUsers';
 import recommandUsersReducer from './slice/getRecommandUsers';
 import timelinePostsReducer from './slice/getTimelinePosts';
 import loginUserReducer from './slice/loginUser';
 
+const authPersistConfig = {
+  key: 'auth',
+  storage: storage,
+  blacklist: ['token'],
+};
+
 const combinedReducer = combineReducers({
-  currentUser: loginUserReducer,
+  currentUser: persistReducer(authPersistConfig, loginUserReducer), //nested persist to exclude token
   followingUsers: followingUsersReducer,
   recommandUsers: recommandUsersReducer,
   timelinePosts: timelinePostsReducer,
@@ -15,6 +23,7 @@ const combinedReducer = combineReducers({
 const rootReducer = (state, action) => {
   if (action.type === 'user/logoutUser') {
     state = undefined;
+    storage.removeItem('persist:auth');
   }
   return combinedReducer(state, action);
 };
